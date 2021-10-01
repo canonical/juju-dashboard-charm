07cd98b6c70d25b3414408fd6859d5e0b7710b6a
João Martins <joao.figueiredo.martins@canonical.com>
2021-10-01 11:37:02 -0400

    Handle missing/incorrect configs and relation values

--- src/charm.py
+++ src/charm.py
@@ -5,18 +5,17 @@
 """Charm the service."""
 
 import os
-import signal
 import logging
-import subprocess
-import shutil
 
 from jinja2 import Environment, FileSystemLoader
 
 from ops.charm import CharmBase
+from ops.model import ActiveStatus, BlockedStatus
 from ops.main import main
 from ops.framework import StoredState
 
 from charmhelpers.core import hookenv;
+
 
 logger = logging.getLogger(__name__)
 
@@ -42,8 +41,7 @@ class JujuDashboardCharm(CharmBase):
         Start the webserver to host the dashboard.
         """
         os.system("apt install -y nginx")
-        self.generate_and_save_config()
-        self.restart_nginx()
+        self._configure()
 
     def _on_start(self, _):
         """
@@ -55,8 +53,7 @@ class JujuDashboardCharm(CharmBase):
         """
         Restart the webserver to host the dashboard.
         """
-        self.generate_and_save_config()
-        self.restart_nginx()
+        self._configure()
 
     def _on_config_changed(self, _):
         """
@@ -64,8 +61,7 @@ class JujuDashboardCharm(CharmBase):
         Any values defined in the configuration will override the values
         provided by the controller relation.
         """
-        self.generate_and_save_config()
-        self.restart_nginx()
+        self._configure()
 
     def _on_dashboard_relation_changed(self, event):
         """
@@ -85,44 +81,50 @@ class JujuDashboardCharm(CharmBase):
         `juju dashboard` command.
         """
         self._stored.controllerData["controller-url"] = event.relation.data[event.app]["controller-url"]
-        self._stored.controllerData["model-url-template"] = event.relation.data[event.app]["model-url-template"]
         self._stored.controllerData["identity-provider-url"] = event.relation.data[event.app].get("identity-provider-url", "")
         self._stored.controllerData["is-juju"] = event.relation.data[event.app]["is-juju"]
-        # Send the data to the controller for our endpoint
-        # XXX get the machines IP address using:
-        #   https://ops.readthedocs.io/en/latest/#ops.model.Network
-        #event.relation.data[event.app]["hostname"] = "0.0.0.0:8080"
-        # XXX render data into the html page
-        self.generate_and_save_config()
-        self.restart_nginx()
 
-    def generate_and_save_config(self):
+        self._configure()
+
+    def _configure(self):
         """
         Take the configuration values and render them to the index.html file
         for display on the static page.
         """
         data = self._stored.controllerData
+
+        controller_url = data.get("controller-url")
+        identity_provider_url = data.get("identity-provider-url")
+        is_juju = data.get("is-juju", True)
+
+        if not controller_url:
+            self.unit.status = BlockedStatus("Missing controller URL")
+            return
+
         env = Environment(loader=FileSystemLoader(os.getcwd()))
         env.filters['bool'] = bool
 
         config_template = env.get_template("src/config.js.template")
         config_template.stream(
-            controller_url=data.get("controller-url"),
             base_app_url="/",
-            identity_provider_url=data.get("identity-provider-url"),
-            is_juju=data.get("is-juju")
+            controller_api_endpoint="/api",
+            identity_provider_url=identity_provider_url,
+            is_juju=is_juju
         ).dump("src/dist/config.js")
 
         nginx_template = env.get_template("src/nginx.conf.template")
         nginx_template = nginx_template.stream(
             # nginx proxy_pass expects the protocol to be https
-            controller_ws_api=data.get("controller-url").replace("wss", "https"),
+            controller_ws_api=controller_url.replace("wss", "https"),
             dashboard_root=os.getcwd()
         ).dump("/etc/nginx/sites-available/default")
 
-    def restart_nginx(self):
-        os.system("sudo service restart nginx")
-
+        nginx_status = os.system("sudo systemctl restart nginx")
+        # If restarting nginx returns a 0 status it should have been succesfull
+        if nginx_status == 0:
+            self.unit.status = ActiveStatus()
+        else:
+            self.unit.status = BlockedStatus("Could not start nginx")
 
 if __name__ == "__main__":
     main(JujuDashboardCharm)
