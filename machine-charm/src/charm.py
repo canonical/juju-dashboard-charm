4dba11107a648abc42b3397ab9cfde851d8a4de4
alesstimec <ales.stimec@canonical.com>
2022-08-31 14:27:35 +0200

    Fixes for the charm
    - ensures the controller url starts with https:// for the nginx config
    - adds ssl to nginx template
    - add dns-name config option

--- machine-charm/src/charm.py
+++ machine-charm/src/charm.py
@@ -2,20 +2,16 @@
 # Copyright 2021 Canonical
 # See LICENSE file for licensing details.
 
-import os
 import logging
-
-from jinja2 import Environment, FileSystemLoader
-
-from ops.charm import CharmBase
-from ops.model import ActiveStatus, BlockedStatus
-from ops.main import main
-from ops.framework import StoredState
-
-from charms.juju_dashboard.v0.juju_dashboard import JujuDashReq
+import os
 
 from charmhelpers.core import hookenv  # FIXME: This needs to be ported to ops.
-
+from charms.juju_dashboard.v0.juju_dashboard import JujuDashData, JujuDashReq
+from jinja2 import Environment, FileSystemLoader
+from ops.charm import CharmBase
+from ops.framework import StoredState
+from ops.main import main
+from ops.model import ActiveStatus, BlockedStatus
 
 logger = logging.getLogger(__name__)
 
@@ -34,6 +30,7 @@ class JujuDashboardCharm(CharmBase):
                                self._on_controller_relation_changed)
         self.framework.observe(self.on["dashboard"].relation_changed,
                                self._on_dashboard_relation_changed)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
         self._stored.set_default(controllerData={})
 
     def _on_install(self, _):
@@ -52,6 +49,12 @@ class JujuDashboardCharm(CharmBase):
 
         self._configure(**requires.data)
 
+    def _on_config_changed(self, _):
+        relation = self.model.get_relation("controller")
+        if relation:
+            data = JujuDashData(relation.data[relation.app])
+            self._configure(data['controller_url'], data['identity_provider_url'], data['is_juju'])
+
     def _configure(self, controller_url, identity_provider_url, is_juju):
         """ """
 
@@ -67,11 +70,20 @@ class JujuDashboardCharm(CharmBase):
             is_juju=is_juju
         ).dump("src/dist/config.js")
 
+        controller_url.replace("wss", "https")
+        if not controller_url.startswith('https://'):
+            controller_url = 'https://{}'.format(controller_url)
+
+        if self.config.get('dns-name'):
+            hookenv.open_port(443)
+            hookenv.close_port(8080)
+
         nginx_template = env.get_template("src/nginx.conf.template")
         nginx_template = nginx_template.stream(
             # nginx proxy_pass expects the protocol to be https
             controller_ws_api=controller_url.replace("wss", "https"),
-            dashboard_root=os.getcwd()
+            dashboard_root=os.getcwd(),
+            dns_name=self.config.get('dns-name')
         ).dump("/etc/nginx/sites-available/default")
 
         nginx_status = os.system("sudo systemctl restart nginx")
