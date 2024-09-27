3bde180e225f358548af5520181e4d5b4cf231d3
vladimir-cucu <vladimir.cucu@canonical.com>
2024-09-27 16:29:43 +0300

    fix: apply same fixes to machine charm

--- machine-charm/lib/charms/juju_dashboard/v0/juju_dashboard.py
+++ machine-charm/lib/charms/juju_dashboard/v0/juju_dashboard.py
@@ -71,10 +71,22 @@ class JujuDashData(Mapping):
                 juju-controller application.
 
         """
+        # Juju controller provides is-juju, while JAAS provides is_juju.
+        is_juju = (
+            data.get("is_juju", True)
+            if data.get("is-juju") is None
+            else data.get("is-juju")
+        )
+        # Juju controller provides controller-url, while JAAS provides controller_url.
+        controller_url = (
+            data.get("controller_url", "")
+            if data.get("controller-url") is None
+            else data.get("controller-url")
+        )
         self._data = {
-            "controller_url": re.sub(r'\/api$', '', data.get("controller-url", "")),
+            "controller_url": re.sub(r'\/api$', '', controller_url),
             "identity_provider_url": data.get("identity-provider-url", ""),
-            "is_juju": data.get("is-juju", True),
+            "is_juju": is_juju,
         }
 
     def __contains__(self, key):
--- machine-charm/src/charm.py
+++ machine-charm/src/charm.py
@@ -94,7 +94,7 @@ class JujuDashboardCharm(CharmBase):
         config_template = env.get_template("src/config.js.template")
         config_template.stream(
             base_app_url="/",
-            controller_api_endpoint="/api",
+            controller_api_endpoint=("" if self._bool(is_juju) else controller_url) + "/api",
             identity_provider_url=identity_provider_url,
             is_juju=is_juju,
             analytics_enabled=self.config.get('analytics-enabled')
@@ -113,7 +113,8 @@ class JujuDashboardCharm(CharmBase):
         nginx_template = nginx_template.stream(
             controller_ws_api=controller_url,
             dashboard_root=os.getcwd(),
-            dns_name=self.config.get('dns-name')
+            dns_name=self.config.get('dns-name'),
+            is_juju=is_juju
         ).dump("/etc/nginx/sites-available/default")
 
         nginx_status = os.system("sudo systemctl restart nginx")
